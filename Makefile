d18141110672bdb5783731e999671900c6de9b68
fedfontana <federico.fontana2000@gmail.com>
2021-05-27 17:08:33 +0200

    updated test stuff

--- Makefile
+++ Makefile
@@ -1,2 +1,7 @@
 clean: 
 	find . -type f -path "./lpp/*/*" -name "*.class" -exec rm -f {} \;
+
+test: clean
+	javac lpp/Main.java
+	python tests.py
+	find . -type f -path "./lpp/*/*" -name "*.class" -exec rm -f {} \;
--- tests.py
+++ tests.py
@@ -1,15 +1,21 @@
 import os
 
 def test(dir_name, ntc = False):
-    print(f"testing {dir_name}")
+    print(f"===| Testing files in the directory {dir_name} |===")
     for f in os.listdir(dir_name):
         if f != 'ntc':
-            print(f"testing {f}")
-            os.system(f"java lpp.Main -i {dir_name}/{f}")
+            print(f"===| Testing {f} {'with ntc' if ntc else ''} |===")
+            os.system(f"java lpp.Main {'-ntc' if ntc else ''} -i {dir_name}/{f}")
 
-dirs=("failure/ntc", "failure/syntax","failure/type","failure/syntax","success/ntc", "success")
+dirs=(  ("failure/ntc", True),
+        ("failure/syntax", False),
+        ("failure/type", False),
+        ("failure/syntax", False),
+        ("success/ntc",  True),
+        ("success", False)
+    )
 
-for dir in dirs:
-    test(f"tests/{dir}")
+for dir, type_check in dirs:
+    test(f"tests/{dir}", ntc= type_check)
 
-print("===Ended Tests===")
+print("===| Ended Tests |===")
