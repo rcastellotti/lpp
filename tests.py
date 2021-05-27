17f6bf9144f5658ecd35a8b809aabd05b18237f3
rcastellotti <me@rcastellotti.dev>
2021-05-27 13:18:08 +0200

    updated python script and a test

--- tests.py
+++ tests.py
@@ -5,9 +5,11 @@ def test(dir_name, ntc = False):
     for f in os.listdir(dir_name):
         if f != 'ntc':
             print(f"testing {f}")
-            os.system(f"java lab11_05_14.Main -i {dir_name}/{f}")
+            os.system(f"java lpp.Main -i {dir_name}/{f}")
 
 dirs=("failure/ntc", "failure/syntax","failure/type","failure/syntax","success/ntc", "success")
 
 for dir in dirs:
     test(f"tests/{dir}")
+
+print("===Ended Tests===")
