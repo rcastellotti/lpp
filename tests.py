f1b698db518be75f1b4d45bab1bf5fa5191751ae
fedfontana <federico.fontana2000@gmail.com>
2021-05-27 17:14:41 +0200

    updated test.py

--- tests.py
+++ tests.py
@@ -1,21 +1,17 @@
 import os
 
-def test(dir_name, ntc = False):
-    print(f"===| Testing files in the directory {dir_name} |===")
+def test(dir_name):
+    ntc = False
+    if 'ntc' in dir_name:
+        ntc = True
     for f in os.listdir(dir_name):
         if f != 'ntc':
-            print(f"===| Testing {f} {'with ntc' if ntc else ''} |===")
+            print(f"===| Running {dir_name}/{f} {'with ntc' if ntc else ''} |===")
             os.system(f"java lpp.Main {'-ntc' if ntc else ''} -i {dir_name}/{f}")
 
-dirs=(  ("failure/ntc", True),
-        ("failure/syntax", False),
-        ("failure/type", False),
-        ("failure/syntax", False),
-        ("success/ntc",  True),
-        ("success", False)
-    )
+dirs = ("failure/ntc", "failure/syntax", "failure/type", "success", "success/ntc") 
 
-for dir, type_check in dirs:
-    test(f"tests/{dir}", ntc= type_check)
+for dir in dirs:
+    test(f"tests/{dir}")
 
 print("===| Ended Tests |===")
